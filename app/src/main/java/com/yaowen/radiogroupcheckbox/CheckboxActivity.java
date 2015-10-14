11eff2312150494c2b4be89d8d9c3f4f2e03e883
茗香墨语 <yaowen424@126.com>
2015-10-14 11:37:34 +0800

    新增一个活动并注册

---
+++ app/src/main/java/com/yaowen/radiogroupcheckbox/CheckboxActivity.java
@@ -0,0 +1,15 @@
+package com.yaowen.radiogroupcheckbox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckboxActivity extends AppCompatActivity {
+    private
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkbox);
+
+    }
+
+}
